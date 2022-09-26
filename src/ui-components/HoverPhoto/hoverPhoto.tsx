455bd4eee16dea244e10f62b97709fef024b6c44
jmuru <jmuruguru94@gmail.com>
2022-09-26 01:20:58 -0400

    comments cleanup

--- src/ui-components/HoverPhoto/hoverPhoto.tsx
+++ src/ui-components/HoverPhoto/hoverPhoto.tsx
@@ -12,32 +12,17 @@ interface HoverPhotoProps {
 
 const HoverPhoto: React.FC<HoverPhotoProps> = props => {
     const [isHovered, toggleHovered] = useState(false)
-    // debug, remove later
     useEffect(() => {
         console.log(isHovered)
     })
 
-    // const defaultStyle = {
-    //     background: `url(${props.url})`,
-    //     height: "200px",
-    //     width: "200px",
-    // }
-    //
-    // const hoverStyle = {
-    //     background: `url(${props.url}), linear-gradient(180deg, rgba(0,0,0,0), black)`,
-    //     height: "200px",
-    //     width: "200px",
-    // }
     return (
         <div
-            // style={isHovered ? hoverStyle : hoverStyle}
             className={`hoverPhotoContainer`}
             onMouseEnter={() => toggleHovered(true)}
             onMouseLeave={() => toggleHovered(false)}
         >
             <img
-                // onMouseEnter={() => toggleHovered(true)}
-                // onMouseLeave={() => toggleHovered(false)}
                 src={props.url}
                 width={`${props.width}px`}
                 height={`${props.height}px`} />
